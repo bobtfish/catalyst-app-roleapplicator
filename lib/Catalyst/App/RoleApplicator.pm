eaee4f7f5ae5165a32270c6c16bdd82071292cbf
t0m <bobtfish@bobtfish.net>
2009-04-17 23:19:47 +0100

    stuff, kinda, fucking weird on 5.8

--- lib/Catalyst/App/RoleApplicator.pm
+++ lib/Catalyst/App/RoleApplicator.pm
@@ -13,8 +13,6 @@ my @attributes = map {
     [ $_, "${_}_class", "${_}_roles" ]
 } qw/
     dispatcher
-    engine
-    context
     request
     response
     stats
@@ -28,37 +26,49 @@ sub init_meta {
     my $meta = find_meta($caller);
     confess 'oh noes' unless $meta;
 
-    $caller->mk_classdata(map { $_->[2] } @attributes);
+    warn $meta->is_immutable;
 
+    $caller->mk_classdata(map { $_->[2] } @attributes);
+    warn("Made " . join(', ', map { $_->[2] } @attributes));
     $meta->add_after_method_modifier(setup_finalize => sub {
+        warn("In setup finalize");
         my ($app) = @_;
         for my $attr (@attributes) {
+            use Data::Dumper;
+            warn("ATTR " . Dumper($attr));
             my $roles = $app->${ \$attr->[2] };
+            warn("Attr $attr roles $roles");
             next unless $roles;
-
-            Class::MOP::load_class($_)
-                for map {
-                    $_ = $app->_transform_role_name($attr->[0], $_)
+            warn("work out LOAD");
+            my @to_load = map {
+                    warn($attr->[0], $_);
+                    $_ = $app->_transform_role_name($attr->[0], $_);
+                    warn("Load $_");
                 } @{ $roles };
-
+            warn("Pre load");
+            Class::MOP::load_class($_) for @to_load;
+            warn("loaded");
             my $superclass = $app->${ \$attr->[1] };
 
             # hack: context_class doesn't have a default until the
             #       first request
             $superclass = $app
                 if $attr->[0] eq 'context' && !$superclass;
-
+            warn("Superclass $superclass");
             my $meta = Class::MOP::Class->create_anon_class(
                 superclasses => [ $superclass ],
                 roles        => $roles,
                 cache        => 1,
             );
+            warn("Made meta $meta " . $meta->name);
             $meta->add_method(meta => sub { $meta });
-
+            warn("Added meta");
             $app->${ \$attr->[1] }($meta->name);
+            warn("Assigned");
         }
+        warn("Finished setup_finalize");
     });
-
+    warn("adding trn method");
     $meta->add_method(_transform_role_name => sub {
         my ($app, $kind, $short) = @_;
         my $part = ucfirst $kind;
@@ -69,6 +79,7 @@ sub init_meta {
             $short,
         );
     });
+    warn("Done");
 }
 
 1;
--- t/lib/TestApp.pm
+++ t/lib/TestApp.pm
@@ -1,14 +1,18 @@
 package TestApp;
 
 use Moose;
-use Catalyst::App::RoleApplicator;
 
 use namespace::clean -except => 'meta';
+BEGIN { # I predict Scope::Upper before we're done here.
 
-extends 'Catalyst';
-
+    extends 'Catalyst';
+}
+require Catalyst::App::RoleApplicator;
+Catalyst::App::RoleApplicator->import;
+__PACKAGE__->mk_classdata('request_roles');
 __PACKAGE__->request_roles([qw/Foo ~Bar/]);
 
 __PACKAGE__->setup;
+warn("AFTER SETUP");
 
 1;
